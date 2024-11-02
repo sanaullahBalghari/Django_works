683c53e2a328eb081d5293c705c5beecc8947aa6
sanaullahBalghari <149436752+sanaullahBalghari@users.noreply.github.com>
2024-11-02 22:28:22 +0500

    Delete Todoapp directory

--- Todoapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Todo(models.Model):
-    title=models.CharField(max_length=50)
-    description = models.TextField() 
-    completed=models.BooleanField(default=False)
-
